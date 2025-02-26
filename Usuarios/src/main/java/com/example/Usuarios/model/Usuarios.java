af36185d38daad03ef38f367459ce2b3b206f2aa
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2025-02-26 19:48:24 +0100

    carmen

---
+++ Usuarios/src/main/java/com/example/Usuarios/model/Usuarios.java
@@ -0,0 +1,18 @@
+package com.example.Usuarios.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "usuarios")
+public class Usuarios {
+    @Id
+    private Integer id;
+    private String nombre;
+    private String rol;
+}
